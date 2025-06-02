d80d15ea73c9ddd926804cee9c32c68e2f97ec45
ZACHARY ONGERI <114331356+zackseal89@users.noreply.github.com>
2025-06-02 08:40:49 +0300

    Set up Clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import { Inter } from 'next/font/google';
-
 import { ClerkProvider } from '@clerk/nextjs';
-
 import { ThemeProvider } from '@/providers/theme-provider';
 import { ModalProvider } from '@/providers/modal-provider';
 import { ToasterProvider } from '@/providers/toast-provider';
-
 import './globals.css';
-
 import type { Metadata } from 'next';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
